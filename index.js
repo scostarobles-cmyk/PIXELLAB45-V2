939db5c536199bf32c083e728f4df5b811fd60ae
scostarobles-cmyk <scostarobles@gmail.com>
2026-06-11 23:19:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 export default {
   async fetch(request) {
-    return new Response("Pixellab45 worker online", {
+    return new Response("Pixellab45 worker online desde github", {
       headers: { "content-type": "text/plain" },
     });
   },
