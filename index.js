9dad0332756e887baf92d98715760b81239ab2b3
scostarobles-cmyk <scostarobles@gmail.com>
2026-06-12 03:48:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,4 @@
-export default {
-  async fetch(request) {
-
-    const cors = {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-      "Access-Control-Allow-Headers": "Content-Type"
-    };
-
-    if (request.method === "OPTIONS") {
-      return new Response(null, { headers: cors });
-    }
-
-    return new Response(
-      JSON.stringify({
-        ok: true,
-        mensaje: "Worker activo"
-      }),
-      {
-        headers: {
-          ...cors,
-          "Content-Type": "application/json"
-        }
-      }
-    );
-
-  }
-};
+return Response.json({
+  ok: true,
+  image_url: "https://picsum.photos/600"
+});
