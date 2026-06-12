90eb23eabb61fced1ed69b0a453d9e02a14f9afd
scostarobles-cmyk <scostarobles@gmail.com>
2026-06-12 04:17:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,84 +1,10 @@
 export default {
   async fetch(request, env) {
 
-    const cors = {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-      "Access-Control-Allow-Headers": "Content-Type"
-    };
-
-    if (request.method === "OPTIONS") {
-      return new Response(null, { headers: cors });
-    }
-
-    if (request.method !== "POST") {
-      return new Response(
-        JSON.stringify({
-          ok: true,
-          mensaje: "PIXELLAB45 Worker activo"
-        }),
-        {
-          headers: {
-            ...cors,
-            "Content-Type": "application/json"
-          }
-        }
-      );
-    }
-
-    try {
-
-      const { prompt } = await request.json();
-
-      const replicateResponse = await fetch(
-        "https://api.replicate.com/v1/models/black-forest-labs/flux-1.1-pro/predictions",
-        {
-          method: "POST",
-          headers: {
-            "Authorization": `Bearer ${env.REPLICATE_API_TOKEN}`,
-            "Content-Type": "application/json",
-            "Prefer": "wait"
-          },
-          body: JSON.stringify({
-            input: {
-              prompt: prompt,
-              aspect_ratio: "1:1",
-              output_format: "webp",
-              output_quality: 80,
-              prompt_upsampling: true
-            }
-          })
-        }
-      );
-
-      const data = await replicateResponse.json();
-
-      return new Response(
-        JSON.stringify(data),
-        {
-          headers: {
-            ...cors,
-            "Content-Type": "application/json"
-          }
-        }
-      );
-
-    } catch (error) {
-
-      return new Response(
-        JSON.stringify({
-          ok: false,
-          error: error.message
-        }),
-        {
-          headers: {
-            ...cors,
-            "Content-Type": "application/json"
-          }
-        }
-      );
-
-    }
+    return Response.json({
+      tokenExiste: !!env.REPLICATE_API_TOKEN,
+      tokenPrimeros5: env.REPLICATE_API_TOKEN?.substring(0,5)
+    });
 
   }
 };
