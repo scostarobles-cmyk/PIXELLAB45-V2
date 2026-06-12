a4897dcab1132e3236adf715be9c998a454f11fe
scostarobles-cmyk <scostarobles@gmail.com>
2026-06-12 04:02:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 export default {
   async fetch(request) {
 
-    return Response.json({
-      ok: true,
-      image_url: "https://picsum.photos/600"
-    });
+    const cors = {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+      "Access-Control-Allow-Headers": "*"
+    };
 
+    if (request.method === "OPTIONS") {
+      return new Response(null, {
+        headers: cors
+      });
+    }
+
+    return new Response(
+      JSON.stringify({
+        ok: true,
+        image_url: "https://picsum.photos/600"
+      }),
+      {
+        headers: {
+          ...cors,
+          "Content-Type": "application/json"
+        }
+      }
+    );
   }
 };
