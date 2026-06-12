28fb03649670bddf62811e602ac452ed6d91e67b
scostarobles-cmyk <scostarobles@gmail.com>
2026-06-12 01:55:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
 
     const cors = {
-      "Access-Control-Allow-Origin": "*",
+      1"Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Methods": "POST, OPTIONS",
       "Access-Control-Allow-Headers": "Content-Type"
     };
