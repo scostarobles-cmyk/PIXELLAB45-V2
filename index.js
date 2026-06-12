8ad0ae12b2a9b581309a0847054a1cd3ef97a62c
scostarobles-cmyk <scostarobles@gmail.com>
2026-06-12 04:26:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 export default {
   async fetch(request, env) {
 
-    return Response.json({
-      tokenExiste: !!env.REPLICATE_API_TOKEN,
-      tokenPrimeros5: env.REPLICATE_API_TOKEN?.substring(0,5)
-    });
+    const response = await fetch(
+      "https://api.replicate.com/v1/account",
+      {
+        headers: {
+          "Authorization": `Token ${env.REPLICATE_API_TOKEN}`
+        }
+      }
+    );
 
+    const data = await response.json();
+
+    return Response.json(data);
   }
 };
