1b4642280fd29c9311952908af7580bf7c989cc7
scostarobles-cmyk <scostarobles@gmail.com>
2026-06-13 10:27:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 export default {
   async fetch(request, env) {
-    return Response.json({
-      aiExiste: !!env.AI,
-      tipo: typeof env.AI
-    });
+    try {
+
+      const imagen = await env.AI.run(
+        "black-forest-labs/flux-2-pro-preview",
+        {
+          prompt: "Robot futurista cyberpunk con luces azules, estilo cinematográfico, ultra detallado",
+          width: 1024,
+          height: 1024
+        }
+      );
+
+      return Response.json(imagen);
+
+    } catch (error) {
+
+      return Response.json({
+        error: error.message
+      });
+
+    }
   }
 }
