13afb4d0b73ac9339c2a280494d19f24ee148acd
scostarobles-cmyk <scostarobles@gmail.com>
2026-06-13 10:23:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,8 @@
 export default {
   async fetch(request, env) {
-    try {
-
-      const respuesta = await env.AI.run(
-        "@cf/meta/llama-3.1-8b-instruct",
-        {
-          messages: [
-            {
-              role: "user",
-              content: "Responde únicamente: PIXELLAB45 conectado"
-            }
-          ]
-        }
-      );
-
-      return Response.json(respuesta);
-
-    } catch (error) {
-
-      return Response.json({
-        error: error.message
-      });
-
-    }
+    return Response.json({
+      aiExiste: !!env.AI,
+      tipo: typeof env.AI
+    });
   }
 }
