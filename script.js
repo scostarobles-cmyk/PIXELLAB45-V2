ffdd294b6b115be30b893446c925392ae97eb784
scostarobles-cmyk <scostarobles@gmail.com>
2026-06-14 07:54:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
-alert("PIXELLAB45 FUNCIONA");
-console.log("PIXELLAB45 OK");
+//generar inagen 
+async function generarIdeas() {
+
+  const resultado = document.getElementById("resultadoIdeas");
+
+  try {
+
+    resultado.innerText = "Generando ideas...";
+
+    const respuesta = await fetch(
+      "https://pixellab45-v2.scostarobles.workers.dev/text",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          prompt: "Genera 10 ideas virales sobre IA"
+        })
+      }
+    );
+
+    const texto = await respuesta.text();
+
+    resultado.innerText = texto;
+
+  } catch (e) {
+    resultado.innerText = "Error: " + e.message;
+  }
+}
