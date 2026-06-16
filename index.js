94a2ec00817da09f4a183c56c93898d0a756f302
scostarobles-cmyk <scostarobles@gmail.com>
2026-06-16 02:50:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -196,6 +196,26 @@ else if (tipo === "guardar-imagen") {
     }
   );
 }
+  // 📂 LISTAR IMÁGENES
+else if (tipo === "listar-imagenes") {
+
+  const objetos = await env.IMAGES.list();
+
+  const imagenes = objetos.objects.map(obj => ({
+    nombre: obj.key,
+    url: `https://pub-e461375551fb4e4086818d0c485c5fd4.r2.dev/${obj.key}`
+  }));
+
+  return new Response(
+    JSON.stringify(imagenes),
+    {
+      headers: {
+        ...corsHeaders,
+        "Content-Type": "application/json"
+      }
+    }
+  );
+    }
 
 // 🖼️ GENERADOR DE IMAGEN
 else if (tipo === "imagen") {
