389c06693497b787858c825d2333ae40cbff8226
scostarobles-cmyk <scostarobles@gmail.com>
2026-06-16 21:01:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -162,7 +162,7 @@ ${estilo}
   return new Response(
     JSON.stringify({
       success: true,
-      mensaje: `Recibido el video en modo: ${body.tipo}, contenido: ${body.contenido}, duración: ${body.duracion}`
+      mensaje: `Recibido el video en modo: ${tipo}, contenido: ${contenido}, duración: ${duracion}`
     }),
     {
       headers: {
