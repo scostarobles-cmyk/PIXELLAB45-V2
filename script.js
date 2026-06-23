58fe323d915bae76668eaa10c13f3455f9ca7fa7
scostarobles-cmyk <scostarobles@gmail.com>
2026-06-23 00:43:22 -0300

    deploy

--- script.js
+++ script.js
@@ -125,6 +125,33 @@ contenedor.innerHTML =
 
 }
 
+// ========================================
+// MENÚ HAMBURGUESA
+// ========================================
+
+function toggleMenu() {
+
+document
+.querySelector(".nav-links")
+.classList
+.toggle("active");
+
+}
+
+document
+.querySelectorAll(".nav-links a")
+.forEach(link => {
+
+link.addEventListener("click", () => {
+
+  document
+    .querySelector(".nav-links")
+    .classList
+    .remove("active");
+
+});
+
+});
 // 
 // ========================================
 // INICIO
