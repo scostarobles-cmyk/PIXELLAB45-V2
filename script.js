56b7517dc8a8490dde3d767e869ffbb67dc73915
scostarobles-cmyk <scostarobles@gmail.com>
2026-06-23 23:40:47 -0300

    upadte

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ const res = await fetch(
 const imagenes =
   await res.json();
 
-contenedor.innerHTML = "";
+contenedor.innerHTML = JSON.stringify(imagenes, null, 2);
 
 imagenes.forEach(img => {
 
