9273f66c2b9f3128df304ad56c9de64ad0f460ce
scostarobles-cmyk <scostarobles@gmail.com>
2026-06-24 22:44:04 -0300

    categoría

--- script.js
+++ script.js
@@ -104,55 +104,6 @@ async function cargarCategoria(categoria) {
 
 }
 
-//Galería por categoría 
-async function cargarCategoria(categoria) {
-
-  const contenedor =
-  document.getElementById("galeriaCategoria");
-
-  contenedor.innerHTML =
-    "⏳ Cargando categoría...";
-
-  try {
-
-    const res = await fetch(WORKER_URL, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        tipo: "listar-categoria",
-        categoria: categoria
-      })
-    });
-
-    const data = await res.json();
-
-    contenedor.innerHTML = "";
-
-  data.images.forEach(img => {
-
-      contenedor.innerHTML += `
-        <div class="project-card">
-          <img
-            src="${img.url}"
-            alt="${img.nombre}">
-        </div>
-      `;
-
-    });
-
-  } catch (error) {
-
-    contenedor.innerHTML =
-      "❌ Error cargando categoría";
-
-    console.error(error);
-
-  }
-
-}
-
 
 //Inicio y Menú 
 document.addEventListener(
