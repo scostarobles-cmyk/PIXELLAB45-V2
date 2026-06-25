24e8512fed9838822e5b05f12741d2ea7526836e
scostarobles-cmyk <scostarobles@gmail.com>
2026-06-24 22:03:24 -0300

    prieba 1

--- index.js
+++ index.js
@@ -43,3 +43,20 @@ export default {
     }
   }
 };
+//Galería completa 
+async function listarImagenes(env, json) {
+
+  const lista = await env.IMAGES.list();
+//está es la corrección 
+  const imagenes = lista.objects.map(obj => ({
+    nombre: obj.key,
+    url: `https://pub-e461375551fb4e4086818d0c485c5fd4.r2.dev/${obj.key}`
+  }));
+
+  return json({
+    success: true,
+    images: imagenes,
+    total: imagenes.length
+  });
+
+}
