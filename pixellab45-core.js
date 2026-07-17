525853695c00f08269a385209ae5129e059439d2
scostarobles-cmyk <scostarobles@gmail.com>
2026-07-17 14:43:20 -0300

    deploy

--- pixellab45-core.js
+++ pixellab45-core.js
@@ -95,6 +95,7 @@ if (monitorBotonera) {
         monitorBotonera.scrollHeight;
 
 }
+}
 //====================================================
 // PIXELLAB45 CORE
 // CONTROL DE VERSIÓN Y LIMPIEZA DE CACHÉ
