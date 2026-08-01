0d3fe01a9a1ceea83b1310207d42461915d9f128
scostarobles-cmyk <scostarobles@gmail.com>
2026-08-01 15:03:02 -0300

    deploy

--- editor.js
+++ editor.js
@@ -2006,7 +2006,7 @@ function editarPortada(){
         "Mostrando únicamente portada",
         "monitorEditor"
     );
-     await  cargarEditor();
+     cargarEditor();
     mostrarControlesPortada();
 
 }
@@ -2442,7 +2442,7 @@ function activarEdicionColorPortada(){
 
 //Aquí va x e y
 
-async function cargarEditor(){
+function cargarEditor(){
 
     monitorPIXELLAB(
         "Editorial",
