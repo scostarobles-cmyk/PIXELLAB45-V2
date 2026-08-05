ccdbf157c5c4a95c1873a8534e8f2eae0af18934
scostarobles-cmyk <scostarobles@gmail.com>
2026-08-04 22:27:10 -0300

    Add files via upload

--- editor.js
+++ editor.js
@@ -5,3 +5,32 @@ monitorPIXELLAB(
     "editor.js cargado correctamente",
     "monitorEditor"
 );
+monitorPIXELLAB(
+    "Editorial",
+    "ok",
+    "Editor",
+    "editor.js cargado correctamente",
+    "monitorEditor"
+);
+
+
+let biblioteca = [];
+
+monitorPIXELLAB(
+    "Editorial",
+    "ok",
+    "Variables",
+    "Variable global biblioteca creada",
+    "monitorEditor"
+);
+
+
+let libro = null;
+
+monitorPIXELLAB(
+    "Editorial",
+    "ok",
+    "Variables",
+    "Variable global libro creada",
+    "monitorEditor"
+);
