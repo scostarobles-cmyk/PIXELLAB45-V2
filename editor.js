7ad29a039b5a5587a7a43c3ccde3f394f40cb08b
scostarobles-cmyk <scostarobles@gmail.com>
2026-08-05 00:17:31 -0300

    Add files via upload

--- editor.js
+++ editor.js
@@ -547,3 +547,33 @@ function generarEditorJSON(ebook){
 
 
 }
+
+function abrirEditorEditorial(projectId){
+
+    monitorPIXELLAB(
+        "Editorial",
+        "proceso",
+        "AbrirEditor",
+        "Entró a abrir editor",
+        "monitorEditor"
+    );
+
+
+    monitorPIXELLAB(
+        "Editorial",
+        "datos",
+        "AbrirEditor",
+        "Libro seleccionado: " + projectId,
+        "monitorEditor"
+    );
+
+
+    monitorPIXELLAB(
+        "Editorial",
+        "ok",
+        "AbrirEditor",
+        "Salió de abrir editor",
+        "monitorEditor"
+    );
+
+}
