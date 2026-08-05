cb2482f6cd94f1cdfb6a84d72c83d1cd1d315f70
scostarobles-cmyk <scostarobles@gmail.com>
2026-08-05 12:39:56 -0300

    Add files via upload

--- editor.js
+++ editor.js
@@ -1322,8 +1322,7 @@ async function cargarPaginaPortada() {
         width: "100%",
         height: "100%",
         display: "block",
-        objectFit: "contain",
-        objectPosition: "center",
+        objectFit: "fill",
         position: "absolute",
         top: "0",
         left: "0"
